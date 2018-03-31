1d76be33fb8a426dcdb704bbf46c8bb211d462c0
Joseph <wsp7822@163.com>
2018-03-31 17:20:24 +0800

    leetcode systemDesign

---
+++ src/main/java/wsp/joseph/ListNode.java
@@ -0,0 +1,15 @@
+package wsp.joseph;
+
+/**
+ * @author Joseph
+ * @version 1.0
+ * @DATE 2018-3-31
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int x){
+        val=x;
+    }
+}
